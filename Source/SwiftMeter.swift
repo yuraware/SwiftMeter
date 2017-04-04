ca20966dee53e1f6b0ce714baeb4652da90e7f73
ykobets <ykobets@icloud.com>
2017-04-04 23:14:40 +0200

    added Time structures

--- Source/SwiftMeter.swift
+++ Source/SwiftMeter.swift
@@ -17,19 +17,59 @@ struct StopWatch {
 
     let startDate: NSDate
 
-    init(with start: NSDate) {
+    /// Time elapsed since start of the stopwatch
+    var elapsedTime: TimeValue?
 
+    init(with start: NSDate) {
         mach_timebase_info(&timebase_info)
         numer = UInt64(timebase_info.numer)
         denom = UInt64(timebase_info.denom)
-
         startDate = start
     }
 
     init() {
         self.init(with: NSDate())
     }
+
+    /// Start a stopwatch instance
+    func start() {
+        //TODO
+    }
+
+    /// Stops countdown. The result is stored
+    func stop() -> TimeValue {
+
+        //TODO
+        return TimeValue(1)
+    }
 }
+
+
+/// Time representation in seconds, milliseconds, nanoseconds
+enum TimeType {
+    case nanoseconds
+    case milliseconds
+    case seconds
+}
+
+/// Time value with type (representation). The milliseconds is default
+struct TimeValue {
+
+    var value = 0
+    var type = TimeType.milliseconds
+
+    init(value: Int, type: TimeType) {
+        self.value = value
+        self.type = type
+    }
+
+    init(_ value: Int) {
+        self.value = value
+        self.type = .milliseconds
+    }
+
+}
+
 
 class SwiftMeter {
 
